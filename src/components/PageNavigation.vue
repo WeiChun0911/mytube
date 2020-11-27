4a3d283da28a6cca440f19d6c5df72bf48c43933
Willy <weichun0911@gmail.com>
2020-11-27 22:39:41 +0800

    make numbers and button work

--- src/components/PageNavigation.vue
+++ src/components/PageNavigation.vue
@@ -1,22 +1,95 @@
 <template>
   <div class="Navigation">
     <ul>
-        <li>1</li>
-        <li v-text="'<<'"></li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li v-text="'>>'"></li>
-        <li>{{200%12+1}}</li>
+        <li v-on:click="currentPage = 1">1</li>
+        <li v-on:click="previousPage(currentPage)" v-text="'<<'"></li>
+        <li v-on:click="setCurrentPage($event)">{{firstNumber}}</li>
+        <li v-on:click="setCurrentPage($event)">{{secondNumber}}</li>
+        <li v-on:click="setCurrentPage($event)">{{thirdNumber}}</li>
+        <li v-on:click="setCurrentPage($event)">{{forthNumber}}</li>
+        <li v-on:click="setCurrentPage($event)">{{fifthNumber}}</li>
+        <li v-on:click="nextPage(currentPage)" v-text="'>>'"></li>
+        <li v-on:click="currentPage = totalPage">{{totalPage}}</li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {  
-  name: 'PageNavigation'
+  name: 'PageNavigation',
+  data(){
+    return{
+        currentPage: 1,
+        totalVideo: 200,
+        videoPerPage: 12
+    }
+  },
+  computed:{
+      totalPage: function(){
+          return (this.totalVideo % this.videoPerPage) + 1
+      },
+      firstNumber: function(){
+        if(this.currentPage >= 3 && this.currentPage <= this.totalPage - 2){
+            return this.currentPage -2;
+        } 
+        if(this.currentPage >= this.totalPage - 2){
+            return this.totalPage -4;
+        }
+        return 1;
+      },
+      secondNumber: function(){
+        if(this.currentPage >= 3 && this.currentPage <= this.totalPage - 2){
+            return this.currentPage -1;
+        } 
+        if(this.currentPage >= this.totalPage - 2){
+            return this.totalPage -3;
+        }
+        return 2;
+      },
+      thirdNumber: function(){
+        if(this.currentPage >= 3 && this.currentPage <= this.totalPage - 2){
+            return this.currentPage;
+        }
+        if(this.currentPage >= this.totalPage - 2){
+            return this.totalPage -2;
+        }
+        return 3;
+      },
+      forthNumber: function(){
+        if(this.currentPage >= 3 && this.currentPage <= this.totalPage - 2){
+            return this.currentPage +1;
+        }
+        if(this.currentPage >= this.totalPage - 2){
+            return this.totalPage -1;
+        }
+        return 4;
+      },
+      fifthNumber: function(){
+        if(this.currentPage >= 3 && this.currentPage <= this.totalPage - 2){
+            return this.currentPage +2;
+        }
+        if(this.currentPage >= this.totalPage - 2){
+            return this.totalPage;
+        }
+        return 5;
+      },
+  },
+  methods:{
+    previousPage: function(currentPage){
+        console.log(currentPage);
+        if(currentPage <=1) return;
+        this.currentPage = currentPage - 1;
+    },
+    nextPage: function(currentPage){
+        console.log(currentPage);
+        if(currentPage >=200%12+1) return;
+        this.currentPage = currentPage + 1;
+    },
+    setCurrentPage: function(event){
+        console.log(event.target.innerText);
+        this.currentPage = parseInt(event.target.innerText);
+    }
+  }
 }
 </script>
 
