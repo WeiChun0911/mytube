e416cc66ff4f8ee0c222267f672a2c3330758d6e
Willy <weichun0911@gmail.com>
2020-11-29 16:20:11 +0800

    add router and favorite page

---
+++ src/Favorite.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>Favorite Page</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Favorite'
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Favorite from './Favorite.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      if(this.currentRoute == '/favorite') return Favorite;
+      return App;
+    }
+  },
+  render (h) {
+    return h(this.ViewComponent)
+  }
+})
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname
+})
